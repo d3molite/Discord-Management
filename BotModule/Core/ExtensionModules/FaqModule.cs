ba0a34390c42e198e55babb9b9ca32d063a0d204
demolite <tobi.brinke@gmail.com>
2022-11-25 13:25:21 +0100

    Added FAQ Module

---
+++ BotModule/Core/ExtensionModules/FaqModule.cs
@@ -0,0 +1,17 @@
+﻿using BotModule.Extensions.Faq;
+using Discord;
+using Serilog;
+
+namespace BotModule.Core;
+
+public sealed partial class DiscordBot
+{
+	private async Task LoadFaqModule(IGuild guild)
+	{
+		Log.Information("Loaded Faq Module for {BotName} in {GuildName}", Name, guild.Name);
+
+		var feedbackModule = _interactionService.Modules.First(x => x.Name == nameof(FaqExtension));
+
+		await _interactionService.AddModulesToGuildAsync(guild, true, feedbackModule);
+	}
+}
---
+++ BotModule/Extensions/Faq/FaqExtension.cs
@@ -0,0 +1,11 @@
+﻿using Discord.Interactions;
+
+namespace BotModule.Extensions.Faq;
+
+public class FaqExtension : InteractionModuleBase
+{
+	[SlashCommand("faq", "Enter a keyword and press enter!")]
+	public async Task Faq(string keyword)
+	{
+	}
+}
